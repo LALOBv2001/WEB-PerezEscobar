f264af10ab639f64b4064e324e3a04ffe5fffab9
LALOBv2001 <79590204+LALOBv2001@users.noreply.github.com>
2022-12-01 08:11:24 -0600

    sesiones

---
+++ Parcial 3/Sesiones/pagina.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+session_start();
+if(!isset($_SESSION['usuario'])|| empty($_SESSION['usuario']))
+{
+    header("Location: ./index.html");
+}
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Pagina pricnipal de aplicacion</h1>
+    <button type="button"> <?php echo $_SESSION['usuario'];?>
+    </button>
+</body>
+
+</html>
