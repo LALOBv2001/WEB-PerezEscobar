8eb6e4ee4543117466a19e1ddc946f14fdf2f01c
LALOBv2001 <79590204+LALOBv2001@users.noreply.github.com>
2022-12-09 00:36:54 -0600

    CRUD

--- Parcial 3/ProyectoFinal/BajaRegistro.php
+++ Parcial 3/ProyectoFinal/BajaRegistro.php
@@ -1,15 +1,16 @@
 <?php
-//echo $id;
-$NombrePiloto = $_POST['NombrePiloto'];
+ $servidor='localhost';
+ $basedatos='BaseWebVuelo';
+ $usuario='root';
+ $password='';
+ $port='3306';
+ $NombrePiloto = $_POST['NombrePiloto'];
 
-$servidor='localhost';
-$basedatos='BaseWebVuelo';
-$usuario='root';
-$password='';
-$port='3306';
-
-$con = mysqli_connect($servidor, $usuario, $password, $basedatos,$port) or die("No se puede conectar");
-$consulta = "delete from vuelo where NombrePiloto ='$NombrePiloto'";
-$registros = mysqli_query($con, $consulta) or die("Problemas al traer los datos");
-
+ 
+ $con = mysqli_connect($servidor, $usuario, $password, $basedatos,$port) or die("No se puede conectar");
+ $consulta = "delete from vuelo where NombrePiloto = '$NombrePiloto'";
+ $registros = mysqli_query($con, $consulta) or die("Problemas al traer los datos");
+ $result= mysqli_fetch_array($registros,MYSQLI_ASSOC);
+    mysqli_close($con);
+    echo json_encode($result)
 ?>
