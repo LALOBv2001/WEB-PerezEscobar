0a299bbe284f023f9e345661b20432900ccc3232
LALOBv2001 <79590204+LALOBv2001@users.noreply.github.com>
2022-12-09 07:22:51 -0600

    CRUD

--- Parcial 3/ProyectoFinal/BajaRegistro.php
+++ Parcial 3/ProyectoFinal/BajaRegistro.php
@@ -8,7 +8,7 @@
  $port='3306';
  
  
- $con = mysqli_connect($servidor, $usuario, $password, $basedatos,$port) or die("No se puede conectar");
+ $con = mysqli_connect($servidor, $usuario, $password, $basedatos, $port) or die("No se puede conectar");
  $consulta = "delete from vuelo where NombrePiloto = '$NombrePiloto'";
  $registros = mysqli_query($con, $consulta) or die("Problemas al traer los datos");
   
